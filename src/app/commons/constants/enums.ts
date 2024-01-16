fef0a30bb96f35b0e41900ba49ddb69fd734a62b
imramo00 <b.ramis.2002@gmail.com>
2024-01-17 04:13:39 +0600

    Commit

---
+++ src/app/commons/constants/enums.ts
@@ -0,0 +1,46 @@
+import { LocalizedSimpleObject, LocalizedSimpleStringObject, SimpleObject } from "../dtos/simple-object.dto";
+
+const enums:any = {
+    MONTHS: {
+        JANUARY: new LocalizedSimpleStringObject('JANUARY', '', '', ''),
+        FEBRUARY: new LocalizedSimpleStringObject('FEBRUARY', '', '', ''),
+        MARCH: new LocalizedSimpleStringObject('MARCH', '', '', ''),
+        APRIL: new LocalizedSimpleStringObject('APRIL', '', '', ''),
+        MAY: new LocalizedSimpleStringObject('MAY', '', '', ''),
+        JUNE: new LocalizedSimpleStringObject('JUNE', '', '', ''),
+        JULY: new LocalizedSimpleStringObject('JULY', '', '', ''),
+        AUGUST: new LocalizedSimpleStringObject('AUGUST', '', '', ''),
+        SEPTEMBER: new LocalizedSimpleStringObject('SEPTEMBER', '', '', ''),
+        OCTOBER: new LocalizedSimpleStringObject('OCTOBER', '', '', ''),
+        NOVEMBER: new LocalizedSimpleStringObject('NOVEMBER', '', '', ''),
+        DECEMBER: new LocalizedSimpleStringObject('DECEMBER', '', '', '')
+    },
+    ROLES: {
+        ADMIN: new LocalizedSimpleStringObject('ADMIN', '', '', 'Admin'),
+        PREMIUM: new LocalizedSimpleStringObject('PREMIUM', '', '', 'Premium'),
+        USER: new LocalizedSimpleStringObject('USER', '', '', 'User')
+    },
+    ACCESS_BOOK: {
+        PUBLIC: new LocalizedSimpleStringObject('PUBLIC', '', '', 'Public'),
+        PRIVATE: new LocalizedSimpleStringObject('PRIVATE', '', '', 'Private')
+    },
+    GENRES: {
+        FANTASY: new LocalizedSimpleStringObject('FANTASY', '', '', ''),
+        SCIENCE_FICTION: new LocalizedSimpleStringObject('SCIENCE_FICTION', '', '', ''),
+        REALISM: new LocalizedSimpleStringObject('REALISM', '', '', ''),
+        ROMANCE: new LocalizedSimpleStringObject('ROMANCE', '', '', ''),
+        DETECTIVE: new LocalizedSimpleStringObject('DETECTIVE', '', '', ''),
+        THRILLER: new LocalizedSimpleStringObject('THRILLER', '', '', ''),
+        HORROR: new LocalizedSimpleStringObject('HORROR', '', '', ''),
+        ADVENTURE: new LocalizedSimpleStringObject('ADVENTURE', '', '', ''),
+        HISTORICAL_FICTION: new LocalizedSimpleStringObject('HISTORICAL_FICTION', '', '', ''),
+        PHILOSOPHY: new LocalizedSimpleStringObject('PHILOSOPHY', '', '', ''),
+        PSYCHOLOGY: new LocalizedSimpleStringObject('PSYCHOLOGY', '', '', ''),
+        BIOGRAPHY: new LocalizedSimpleStringObject('BIOGRAPHY', '', '', ''),
+        HUMOR: new LocalizedSimpleStringObject('HUMOR', '', '', ''),
+        DRAMA: new LocalizedSimpleStringObject('DRAMA', '', '', ''),
+        POETRY: new LocalizedSimpleStringObject('POETRY', '', '', ''),
+        EDUCATIONAL: new LocalizedSimpleStringObject('EDUCATIONAL', '', '', ''),
+        OTHER: new LocalizedSimpleStringObject('OTHER', '', '', ''),
+    }
+};
